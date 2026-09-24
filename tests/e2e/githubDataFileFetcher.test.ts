baefceb4160bafe54c86a537691fe37491136583
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2026-09-24 15:32:18 -0400

    Refactor test cases for GitHub data file fetcher

--- tests/e2e/githubDataFileFetcher.test.ts
+++ tests/e2e/githubDataFileFetcher.test.ts
@@ -4,10 +4,12 @@ import { makeGithubClient, predict } from "../../src/index.js";
 
 const github = makeGithubClient();
 
-const CASES = [["thekevinscott/github-data-file-fetcher", 3]] as const;
+const OWNER = 'thekevinscott';
+
+const CASES = [["github-data-file-fetcher", 3]] as const;
 
 test.each(CASES)("%s#%i still predicts the committed list", async (repo, pr) => {
-  const url = new URL(`./responses/${repo.split("/")[1]}/${pr}/fixture.json`, import.meta.url);
+  const url = new URL(`./responses/${OWNER}/${repo}/${pr}/fixture.json`, import.meta.url);
   const expected = JSON.parse(readFileSync(url, "utf8")) as string[];
 
   const { checkNames } = await predict(github, repo, pr);
